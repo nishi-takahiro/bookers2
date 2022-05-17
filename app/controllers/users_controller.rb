4cb8d377ed0d83edba0ba3d0fdc493fba2c535fe
EC2 Default User <ec2-user@ip-172-31-92-64.ec2.internal>
2022-05-17 05:41:19 +0000

    log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
+  
   def new
+   @user = User.new
+  end
+  
+  def creat
   end
 
   def index
   end
-
+  
   def show
+    @user = User.find(params[:id])
   end
-
-  def edit
-  end
+  
+  private
+ 
+ def user_params
+   params.require(:user).permit(:name, :introduction, :image)
+ end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,7 @@ class User < ApplicationRecord
   
   has_one_attached :image
   has_many :books, dependent: :destroy
-  
-  
-  
+ 
   has_one_attached :profile_image
   
   def get_profile_image
